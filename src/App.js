f9da6f4fa1ed16a7a63bb5bc737c8ede3c5aff32
coderperfect <mrityunjoynath38@gmail.com>
2021-02-26 18:03:42 +0530

    Menu for admin added

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React, {Component} from 'react';
+import {Route, Switch} from 'react-router-dom';
+import MenuComponent from './components/menucomponent';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: true,
+      user: null
+    }
+  }
+
+  logout = () => {
+    this.setState({
+      isLoggedIn: false,
+      user: null
+    })
+  }
+
+  login = (user) => {
+    this.setState({
+      isLoggedIn: true,
+      user: user
+    })
+  }
+
+
+ render() {
+    return (
+      <div className="App">
+        <MenuComponent isLoggedIn={this.state.isLoggedIn} logout={this.logout}/>
+        
+        {/* <Switch>
+          <Route 
+            path="/" exact
+            render={
+              this.state.isLoggedIn
+              ? (props) => <CompaniesListComponent {...props} isLoggedIn={this.state.isLoggedIn} user={this.state.user}/>
+              : (props) => <LoginComponent {...props} login={this.login}/>
+            }
+          />
+        </Switch> */}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/menucomponent.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const MenuComponent = (props) => {
+    const isLoggedIn = props.isLoggedIn;
+
+    const loggedInMenu = (
+        <ul className="navbar-nav">
+            <li className="nav-item">
+                <Link className="nav-link" to="/registration-approvals">Registrations Approvals</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/update-test-results">Update Test Results</Link>
+            </li>
+            <li className="nav-item">
+                <Link className="nav-link" to="/" onClick={props.logout}>Logout</Link>
+            </li>
+        </ul>
+    );
+
+    const notLoggedInMenu = (
+        <ul className="navbar-nav">
+            <li className="nav-item">
+                <Link className="nav-link" to="/login">Login</Link>
+            </li>
+        </ul>
+    );
+
+    let menu = null;
+
+    if(isLoggedIn)
+        menu = loggedInMenu;
+    else
+        menu = notLoggedInMenu;
+
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <Link className="navbar-brand" to="/">Patient Tracker</Link>
+
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                {menu}
+            </div>
+        </nav>
+    );
+}
+
+export default MenuComponent;
